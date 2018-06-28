ba0e18b3c3d4f20f32c4293c394d891eca803f2d
Natalja Dembek <opossum@MacBook-Pro.localdomain>
2018-06-28 21:36:38 +0100

    fade in fade out trial

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var germany = document.getElementById('germany')
 var france = document.getElementById('france')
 var spain = document.getElementById('spain')
+// var bigFacts = document.getElementById('bigFacts')
+var bigFacts = document.getElementsByClass('bigFacts')
 
 window.onscroll = function(event){
   window.requestAnimationFrame(onScroll)
@@ -25,13 +27,32 @@ window.onload = function(){
   },1200)
 }
 
-function onScroll() {
-  var scrollPosition = window.pageYOffset
-  germany.style.opacity = scrollPosition/100
-  // console.log('im scrolling')
-}
+// function onScroll() {
+//   var scrollPosition = window.pageYOffset
+//   bigFacts.style.opacity = scrollPosition/500
+//   // console.log('im scrolling')
+// }
 
 function animateIn(elem){
   elem.style.opacity = 1
   elem.style.transform = 'translateX(10px)'
+}
+
+function fadeOut(scroll, start, end, i){
+
+	var y = scroll - start
+	var range = end - starts
+
+	bigFacts[i].style.opacity = 1
+  //'translateX(' + mapInt(scroll, start, end, 0, 50) + 'vw)'
+}
+
+
+function fadeIn(scroll, start, end, i){
+	var y = scroll - start
+	var range = end - start
+
+	bigFacts[i].style.opacity = 0
+
+  //'rgb(' + mapInt(y, 0, range, 255, 0) + ',' + mapInt(y, 0, range, 255, 0) + ',' +  mapInt(y, 0, range, 255, 0) + ')'
 }
