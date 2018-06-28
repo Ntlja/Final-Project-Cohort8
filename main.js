fd04148756a48b8b45eed40e2690e0619d152dde
A Vidmar <a.v.vidmar@gmail.com>
2018-06-29 00:38:54 +0100

    js fix - ClassName not just Class

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var germany = document.getElementById('germany')
 var france = document.getElementById('france')
 var spain = document.getElementById('spain')
 // var bigFacts = document.getElementById('bigFacts')
-var bigFacts = document.getElementsByClass('bigFacts')
+var bigFacts = document.getElementsByClassName('bigFacts')
 
 window.onscroll = function(event){
   window.requestAnimationFrame(onScroll)
@@ -27,32 +27,19 @@ window.onload = function(){
   },1200)
 }
 
-// function onScroll() {
-//   var scrollPosition = window.pageYOffset
-//   bigFacts.style.opacity = scrollPosition/500
-//   // console.log('im scrolling')
-// }
+function onScroll() {
+  // var scrollPosition = window.pageYOffset
+  // bigFacts.style.opacity = scrollPosition/500
+  console.log('im scrolling')
+}
 
 function animateIn(elem){
   elem.style.opacity = 1
   elem.style.transform = 'translateX(10px)'
 }
 
-function fadeOut(scroll, start, end, i){
-
-	var y = scroll - start
-	var range = end - starts
-
-	bigFacts[i].style.opacity = 1
-  //'translateX(' + mapInt(scroll, start, end, 0, 50) + 'vw)'
+function animateOut(elem){
+  elem.style.opacity = 0
+  elem.style.transform = 'translateX(10px)'
 }
 
-
-function fadeIn(scroll, start, end, i){
-	var y = scroll - start
-	var range = end - start
-
-	bigFacts[i].style.opacity = 0
-
-  //'rgb(' + mapInt(y, 0, range, 255, 0) + ',' + mapInt(y, 0, range, 255, 0) + ',' +  mapInt(y, 0, range, 255, 0) + ')'
-}
